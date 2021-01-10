d18a2a3903f36e70cf892fb92f5ccaadde5e40ce
derek <57004310+hack12-web@users.noreply.github.com>
2021-01-10 10:48:50 +0200

    XXX
    nami industries international

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTutorial
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String usersName = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Title = "Hello World";
+            this.WindowStartupLocation = WindowStartupLocation.CenterScreen;
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            Title = e.GetPosition(this).ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Text = "7";
+        }
+    }
+}
